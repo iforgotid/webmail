f92b9ab61fdd0195f7dc0d8c44bd766e25d8b059
Philipp <philipp.xue@gmail.com>
2015-06-17 17:41:12 +0800

    JSON API ready

--- web.py
+++ web.py
@@ -1,9 +1,8 @@
 __author__ = 'philipp'
 import webapp2
 from webapp2_extras import jinja2
-import leancloud
+import leancloud,json
 import os, sys
-
 sys.path.append(os.path.join(os.path.abspath('.'),'model'))
 from Mail import Mail
 leancloud.init('73b6c6p6lgs8s07m6yaq5jeu7e19j3i3x7fdt234ufxw9ity', 'h5lu7ils6mutvirgrxeodo6xfuqcgxh4ny0bdar3utl076cu')
@@ -28,7 +27,20 @@ class SendMailPage(BaseHandler):
         newMail.save()
         context = {'message': 'Hello, world!'}
         self.render_response('main.html', **context)
+class GetJson(BaseHandler):
+    def get(self):
+        self.response.headers['Content-Type'] = 'application/json'
+        obj = {
+            'success': 'some var',
+            'payload': 'some var',
+        }
+        self.response.out.write(json.dumps(obj))
+    def post(self):
+        self.response.headers['Content-Type'] = 'application/json'
+        data = json.loads(self.request.body)
+        self.response.out.write(json.dumps(data))
 
 app = webapp2.WSGIApplication([
     ('/', SendMailPage),
+    ('/json/foo',GetJson)
 ], debug=True)
