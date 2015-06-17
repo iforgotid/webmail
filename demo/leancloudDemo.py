12f142df751062f195427cbc745079b339c0a902
Philipp <philipp.xue@gmail.com>
2015-06-17 15:35:16 +0800

    引入leancloud包

---
+++ demo/leancloudDemo.py
@@ -0,0 +1,13 @@
+import leancloudDemo
+#from leancloud import User
+
+leancloudDemo.init('73b6c6p6lgs8s07m6yaq5jeu7e19j3i3x7fdt234ufxw9ity', 'h5lu7ils6mutvirgrxeodo6xfuqcgxh4ny0bdar3utl076cu')
+
+#user = User()
+#user.set("username", "my name")
+#user.set("password", "my pass")
+#user.set("email", "email@example.com")
+#user.set("phone", "415-392-0202")
+#user.sign_up()
+
+
